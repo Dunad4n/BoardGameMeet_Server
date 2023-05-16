c2b626332d444fbb777c496083406209ff819b03
Dunadan <sosiska7150@gmail.com>
2023-05-16 19:56:05 +0300

    add authorization to swagger

--- src/main/kotlin/com/example/bgm/config/OpenApiConfig.kt
+++ src/main/kotlin/com/example/bgm/config/OpenApiConfig.kt
@@ -2,30 +2,30 @@ package com.example.bgm.config
 
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import lombok.RequiredArgsConstructor
-import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 
-//@RequiredArgsConstructor
-//@Configuration
-//class OpenApiConfig {
-//    fun customOpenAPI(): OpenAPI {
-//        return OpenAPI()
-//            .info(
-//                Info()
-//                    .termsOfService("https://springdoc.org/%22)")
-//                    .components(
-//                        Components()
-//                            .addSecuritySchemes(OAUTH2_CLIENT_CREDENTIALS_KEY, clientCredentialSecurityScheme())
-//                            .addSecuritySchemes(BEARER_TOKEN_KEY, bearerTokenSecurityScheme())
-//                    )
-//                    .addSecurityItem(SecurityRequirement().addList(OAUTH2_CLIENT_CREDENTIALS_KEY))
-//                    .addSecurityItem(SecurityRequirement().addList(BEARER_TOKEN_KEY))
-//    }
-//}
+@RequiredArgsConstructor
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun api(): OpenAPI {
+        return OpenAPI()
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        "bearer-key",
+                        SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                    )
+            )
+            .info(
+                Info().title("Backend of University timetable app")
+                    .version(0.1.toString())
+            )
+    }
+}
