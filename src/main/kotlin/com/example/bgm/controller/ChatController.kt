0a53c924e9e55d24dfbd668de4ecefc52859b286
Dunadan <sosiska7150@gmail.com>
2023-06-02 10:52:30 +0300

    change chat response

--- src/main/kotlin/com/example/bgm/controller/ChatController.kt
+++ src/main/kotlin/com/example/bgm/controller/ChatController.kt
@@ -3,6 +3,7 @@ package com.example.bgm.controller
 import com.example.bgm.controller.dto.CreateMessageRequestEntity
 import com.example.bgm.controller.dto.MessageResponseEntity
 import com.example.bgm.jwt.JwtPerson
+import com.example.bgm.repositories.PersonRepo
 import com.example.bgm.services.MessageService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.messaging.handler.annotation.MessageMapping
@@ -19,6 +20,9 @@ class ChatController {
     @Autowired
     private lateinit var messageService: MessageService
 
+    @Autowired
+    private lateinit var personRepo: PersonRepo
+
     @MessageMapping("/chat")
 //    @SendTo("/topic/chat")
     fun chatting(
@@ -28,10 +32,14 @@ class ChatController {
         println(messageRequestEntity.eventId)
         println(messageRequestEntity.personNickname)
         val message = messageService.createMessage(messageRequestEntity)
+        val person = personRepo.findByNickname(messageRequestEntity.personNickname)
+            ?: throw Exception("person with nickname ${messageRequestEntity.personNickname} not exist")
         val map = mutableMapOf<String, String>()
         map["text"] = message.text
         map["eventId"] = messageRequestEntity.eventId.toString()
         map["nickname"] = messageRequestEntity.personNickname
+        map["name"] = person.name
+        map["avatarId"] = person.avatarId.toString()
         return map
     }
 
